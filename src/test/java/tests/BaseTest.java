33430e0e1303afde9b65fac5aa137970bc6963b1
BenaiminMroz <doFinansowanie911025>
2023-06-09 22:03:44 +0200

    baseTest RegisterTest

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,8 +1,12 @@
 package pl.testeroprogramowania.tests;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import pl.testeroprogramowania.utils.DriverFactory;
 
 import java.util.concurrent.TimeUnit;
@@ -10,7 +14,21 @@ import java.util.concurrent.TimeUnit;
 public class BaseTest {
 
     protected WebDriver driver;
+    protected static ExtentReports extentReports;
+    protected static ExtentHtmlReporter htmlReporter;
 
+    @BeforeSuite
+    public void beforeSuite(){
+        htmlReporter = new ExtentHtmlReporter("index.html");
+        extentReports = new ExtentReports();
+        extentReports.attachReporter(htmlReporter);
+    }
+
+    @AfterSuite
+    public void afterSuite(){
+        htmlReporter.flush();
+        extentReports.flush();
+    }
     @BeforeMethod
     public void setup() {
         driver = DriverFactory.getDriver();
